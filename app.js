f3291309e2d6d000d06e65be5513fbf3c73fa869
sbassalian <solomonbassalian@gmail.com>
2021-05-04 09:52:44 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Jello")
+});
+
+const port = process.env.PORT || 9000;
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
