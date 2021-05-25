5d33af47403e2a453370b09872e0f6add41d8e08
sbassalian <solomonbassalian@gmail.com>
2021-05-25 16:49:28 -0400

    praying

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,5 +1,6 @@
 
-// import React from 'react';
+
+import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
@@ -8,40 +9,15 @@ import MainPageContainer from './main/main_page_container'
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import Home from './home/home_page';
-const path = require('path');
 
 
-// // import videoplayer from './videoplayer';
-// // import notifications from './notifications';
-// // import options from './options';
-
-// const App = () => (
-//   <div>
-//     <NavBarContainer />
-
-//     <Switch>
-//       <AuthRoute exact path="/" component={MainPage} />
-//       <AuthRoute exact path="/login" component={LoginFormContainer} />
-//       <AuthRoute exact path="/signup" component={SignupFormContainer} />
-//       <ProtectedRoute exact path="/home" component={HomePage} />
-      
-//     </Switch>
-//   </div>
-// );
-
-// export default App;
-
-import React from 'react';
 
 
-import VideoPlayer from './videoplayer';
-
-import Notifications from './notifcations';
 
 
 
 const App = () => {
-
+  
   return (
     <div>
 
